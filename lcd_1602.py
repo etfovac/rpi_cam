b1d08d1b18c666313809a1844a4bb0207007694e
etfovac <etfovac@yahoo.com>
2020-12-09 00:42:34 +0100

    Class, end_event, hotkeys changed

--- lcd_1602.py
+++ lcd_1602.py
@@ -32,121 +32,138 @@ sys.path.append(os.path.abspath(".."))
 import config.printout_info as cpi
 import config.printout_format as cpf
 
+class LCD():
+    def __init__(self, lcd_columns=16, lcd_rows=2):
+        # Raspberry Pi Pin Config:
+        lcd_rs = digitalio.DigitalInOut(board.D26)
+        lcd_en = digitalio.DigitalInOut(board.D19)
+        lcd_d7 = digitalio.DigitalInOut(board.D13)
+        lcd_d6 = digitalio.DigitalInOut(board.D6)
+        lcd_d5 = digitalio.DigitalInOut(board.D5)
+        lcd_d4 = digitalio.DigitalInOut(board.D22)
+        lcd_backlight = digitalio.DigitalInOut(board.D27)
+        # a NPN transistor's Base switches the LED backlight on/off
+        
+        # Init lcd class obj
+        self.LCD = characterlcd.Character_LCD_Mono(
+            lcd_rs, lcd_en,
+            lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+            lcd_columns, lcd_rows, lcd_backlight
+        )
+        self.LCD.text_direction = self.LCD.LEFT_TO_RIGHT
+        self.LCD.backlight = True
+        self.LCD.clear()
+        self.LCD.blink = True
+        self.lcd_msg("<Setup>...")
+        self.LCD.cursor = True
+        
+        self.msg_list = cpi.lcd_msg_list(lcd_columns, lcd_rows)
 
-def lcd_setup(lcd_columns=16, lcd_rows=2):
-    # Raspberry Pi Pin Config:
-    lcd_rs = digitalio.DigitalInOut(board.D26)
-    lcd_en = digitalio.DigitalInOut(board.D19)
-    lcd_d7 = digitalio.DigitalInOut(board.D13)
-    lcd_d6 = digitalio.DigitalInOut(board.D6)
-    lcd_d5 = digitalio.DigitalInOut(board.D5)
-    lcd_d4 = digitalio.DigitalInOut(board.D22)
-    lcd_backlight = digitalio.DigitalInOut(board.D27)
-    # a NPN transistor's Base switches the LED backlight on/off
-       
-    # Init lcd class obj
-    lcd = characterlcd.Character_LCD_Mono(
-        lcd_rs, lcd_en,
-        lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-        lcd_columns, lcd_rows, lcd_backlight
-    )
-    lcd.text_direction = lcd.LEFT_TO_RIGHT
-    lcd.backlight = True
-    lcd.clear()
-    lcd.blink = True
-    lcd_msg(lcd, "<Setup>...")
-    lcd.cursor = True
-    msg_list = cpi.lcd_msg_list(lcd_columns, lcd_rows)
-    return lcd, msg_list
+        self.printout_threads_setup([1,6])
+        self.keyboard_listener_setup()
 
-def lcd_msg(lcd, msg_str):
-    lcd.clear()
-    sleep(0.1)
-    lcd.message = msg_str
-    print(lcd.message)
 
-def lcd_printout(lcd, msg_list, delay):
-    # print(msg_list)
-    try:
-        for msg in msg_list:
-            lcd_msg(lcd, msg)
+    def lcd_msg(self, msg):
+        """Prints msg_str to terminal and LCD.
+        LCD is first cleared.
+        Args:
+            msg (string): string formatted by printout_format.py into 2 rows of 16 chars
+        """
+        self.LCD.clear()
+        sleep(0.1)
+        self.LCD.message = msg
+        print(msg)
+
+    def lcd_printout(self, ix, delay):
+
+        for msg in self.msg_list[ix]:
+            self.lcd_msg(msg)
             sleep(delay)
-    except KeyboardInterrupt:
-        print('<CTRL-C> Printout cancelled. Press CTRL-C to stop execution.')
 
-def lcd_printout_timeout(lcd, msg_list, event, timeout_sec):
-    # Triggered on event timeout
-    cntr = 0
-    while not event.isSet():
-        cntr += cntr
-        # print(cntr) #=0 => e.wait works
-        event_is_set = event.wait(timeout_sec)
-        if event_is_set:
-            lcd_printout(lcd,msg_list,1.5)
-            event.clear()  # clear isSet flag in event
-        else:
-            lcd_msg(lcd, cpf.msg_form(cpi.lcd_timestamp()[0:2]))
+    def lcd_printout_timeout(self, end_event, event, timeout_sec=1):
+        # Triggered on event timeout
+        cntr = 0
+        while (not event.isSet()) and (not end_event.isSet()):
+            
+            cntr += cntr
+            # print(cntr) #=0 => e.wait works
+            event_is_set = event.wait(timeout_sec)
+            if end_event.isSet(): break
+            if event_is_set:
+                self.lcd_printout(0,1.5)
+                event.clear()  # clear isSet flag in event
+            else:
+                self.lcd_msg(cpf.msg_form(cpi.lcd_timestamp()[0:2]))
 
-         
-def lcd_printout_timeout2(lcd,msg_list,event,timeout_sec):
-    # Triggered on event timeout
-    while not event.isSet():
-        event_is_set = event.wait(timeout_sec)
-        if not(event_is_set):  # periodic remainder
-            lcd_printout(lcd,msg_list,3)
+            
+    def lcd_printout_timeout2(self, end_event, event, timeout_sec=6):
+        # Triggered on event timeout
+        while (not event.isSet()) and (not end_event.isSet()):
+            
+            event_is_set = event.wait(timeout_sec)
+            if end_event.isSet(): break
+            if not(event_is_set):  # periodic remainder
+                self.lcd_printout(1,3)
 
+    def on_press(self,key):
+        # * global lcd_event_print  # couldn't add param to this callback
+        # Keyboard interupt triggers thread event:
+        if key == keyboard.Key.insert:
+            self.lcd_event_print.set()
 
-lcd_event_print = threading.Event()
-lcd_event_print2 = threading.Event()  # not used
+    #     try:
+    #         print('alphanumeric key {0} pressed'.format(key.char))
+    #     except AttributeError:
+    #         print('special key {0} pressed'.format(key))
 
+    def on_release(self,key):
+        if key == keyboard.Key.esc:
+            print('{0} released - Stopping the keyboard listener'.format(key))
+            return False
+        if key == keyboard.Key.end:
+            self.end()
+            return False
 
-def on_press(key):
-    global lcd_event_print  # couldn't add param to this callback
-    # Keyboard interupt triggers thread event:
-    if key == keyboard.Key.page_down:
-        lcd_event_print.set()
+    def printout_threads_setup(self, timeout_sec=[1,6]):
+        self.lcd_event_print = threading.Event()
+        self.lcd_event_print2 = threading.Event()
+        self.lcd_event_end = threading.Event()
+        #lcd_thread = threading.Thread(target=lcd_printout, args=())
+        #lcd_thread.start()
+        self.lcd_thread = threading.Thread(name='non-blocking P1',
+                                    target = self.lcd_printout_timeout,
+                                    args = (self.lcd_event_end, self.lcd_event_print,
+                                            timeout_sec[0]))
+        self.lcd_thread2 = threading.Thread(name='non-blocking P2',
+                                    target=self.lcd_printout_timeout2,
+                                    args=(self.lcd_event_end, self.lcd_event_print2,
+                                          timeout_sec[1]))
 
-#     try:
-#         print('alphanumeric key {0} pressed'.format(key.char))
-#     except AttributeError:
-#         print('special key {0} pressed'.format(key))
+    def printout_threads_start(self):
+        self.lcd_thread.start()
+        self.lcd_thread2.start()
 
-def on_release(key):
-    if key == keyboard.Key.esc:
-        print('{0} released - Stopping the keyboard listener'.format(key))
-        return False
+    def keyboard_listener_setup(self):
+        self.listener = keyboard.Listener(on_press = self.on_press,
+                                          on_release = self.on_release)
+
+    def keyboard_listener_start(self):
+        self.listener.start()
+        self.listener.wait()
+
+    def end(self):
+        self.listener.stop()
+        self.lcd_event_end.set()  # Send End event to LCD printouts
+        self.lcd_thread.join()
+        self.lcd_thread2.join()
+        self.LCD.backlight = False  # Turns off the LED backlight
+        sys.exit(0)
+
         
 def main():
-    
-    def interrupt_signal_handler(signum, frame):
-        print('Interrupt signal ' + str(signum) +
-              ' on line ' + str(frame.f_lineno) +
-              ' in ' + frame.f_code.co_filename)
-        listener.stop()
-        lcd.backlight = False # Turns off the LED backlight
-        sys.exit(0)
-    
-    [lcd,msg_lists] = lcd_setup()
-    
-    #lcd_thread = threading.Thread(target=lcd_printout, args=())
-    #lcd_thread.start()
-    timeout_sec = [1,6]
-    lcd_thread = threading.Thread(name='non-blocking',
-                                       target = lcd_printout_timeout,
-                                       args = (lcd,msg_lists[0],lcd_event_print,timeout_sec[0]))
-    lcd_thread2 = threading.Thread(name='non-blocking',
-                                   target=lcd_printout_timeout2,
-                                   args=(lcd,msg_lists[1],lcd_event_print2,timeout_sec[1]))
-    lcd_thread.start()
-    lcd_thread2.start()
-    
-    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-    listener.start()
-    listener.wait()
-    
-    signal.signal(signal.SIGINT, interrupt_signal_handler)
-    # Terminal interrupt signal
-    signal.pause()
-    
+    LCD_ref = LCD()
+    LCD_ref.printout_threads_start()
+    LCD_ref.keyboard_listener_start()
+  
 if __name__ == "__main__":
     main()   
